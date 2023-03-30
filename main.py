068ffdf7edb5ec9eea44c50a10ac691ffcd865d1
Cu-Te <pavel.gikov2@gmail.com>
2023-03-30 21:24:00 +0300

    add util

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import view
 import logic
+import util
 
 def main():
     while True:
@@ -8,15 +9,15 @@ def main():
             break
         else:
             view.write("invalid data, try again")
-    ls = create_list(size)
+    ls = util.create_list(size)
 
-    rnd_init_list(ls)
-    user_init_list(ls)
+    #rnd_init_list(ls)
+    util.user_init_list(ls)
 
     second = logic.find_second_max_value(ls)
 
     msg=f"Second max value {second}"
-
+    view.write(ls)
     view.write(msg)
 
 if __name__ == "__main__":
